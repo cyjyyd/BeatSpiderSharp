1d4a07fbba778b7a54658d080d78111ae4e50db5
qe201020335 <qe201020335@sina.com>
2026-03-19 23:29:17 -0400

    Minor improvement to filter options

--- BeatSpiderSharp.Core/Models/Preset/FilterOptions/Options.cs
+++ BeatSpiderSharp.Core/Models/Preset/FilterOptions/Options.cs
@@ -10,15 +10,10 @@ public class Option
     public static implicit operator bool(Option option) => option.Enable;
 }
 
-public class Option<T> : Option
+public class Option<T>(T defaultValue) : Option
 {
     [JsonProperty(Order = -89)]
-    public T Filter { get; set; }
-
-    public Option(T defaultValue)
-    {
-        Filter = defaultValue;
-    }
+    public T Filter { get; set; } = defaultValue;
 }
 
 public class RangeOption<T> : Option where T : struct, IComparable<T>
@@ -26,17 +21,18 @@ public class RangeOption<T> : Option where T : struct, IComparable<T>
     public T? Min { get; set; }
     public T? Max { get; set; }
 
-    public bool InRange(T value) => (!Min.HasValue || value.CompareTo(Min.Value) >= 0) &&
-                                    (!Max.HasValue || value.CompareTo(Max.Value) <= 0);
+    public bool InRange(T? value) => value.HasValue && (!Min.HasValue || value.Value.CompareTo(Min.Value) >= 0) &&
+                                     (!Max.HasValue || value.Value.CompareTo(Max.Value) <= 0);
 }
 
 public class LogicIncludeOption<T>() : Option<ISet<T>>(new HashSet<T>())
 {
     public bool IsOr { get; set; }
-        
+
     public bool SatisfiedBy(ICollection<T> values)
     {
         var required = Filter;
+        if (required.Count == 0) return true; // vacuously true
         return IsOr ? required.Any(values.Contains) : required.All(values.Contains);
     }
 }
